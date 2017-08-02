4db7716c785f46ea3f7ae4d1822e8e6e33afad4f
ksee <wosyou@qq.com>
2017-08-02 18:04:51 +0800

    小八月投票测试文件

--- src/main/java/com/kee/interceptor/MyInterceptor.java
+++ src/main/java/com/kee/interceptor/MyInterceptor.java
@@ -13,6 +13,7 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
         return false;
     }
 
